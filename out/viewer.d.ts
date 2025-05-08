61530b25b3d3cb30f8ff4595d5d6eb18eb33e0a8
mosure <mitchell@mosure.me>
2025-05-08 02:43:10 +0000

    Deploying to www from @ mosure/bevy_zeroverse@b16dafdc109350c5eaee82965d299efaa9d67c7e 🚀

--- out/viewer.d.ts
+++ out/viewer.d.ts
@@ -36,9 +36,9 @@ export interface InitOutput {
   readonly __wbindgen_export_6: WebAssembly.Table;
   readonly closure8115_externref_shim: (a: number, b: number, c: any) => void;
   readonly closure74015_externref_shim: (a: number, b: number, c: any) => void;
-  readonly closure74070_externref_shim: (a: number, b: number, c: any, d: any) => void;
-  readonly closure74074_externref_shim: (a: number, b: number, c: any) => void;
-  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h17d11c90a8c4aeb3: (a: number, b: number) => void;
+  readonly closure74070_externref_shim: (a: number, b: number, c: any) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h82bf0053d5b64c6d: (a: number, b: number) => void;
+  readonly closure74079_externref_shim: (a: number, b: number, c: any, d: any) => void;
   readonly __wbindgen_start: () => void;
 }
 
